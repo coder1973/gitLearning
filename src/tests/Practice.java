26811ace94e9893a51a86cb6a9b0bfd7e85e6347
coder1973 <62132667+coder1973@users.noreply.github.com>
2022-01-10 00:22:38 +0100

    Up

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,6 +5,8 @@ public class Practice {
     public static void main(String[] args) {
 
         System.out.println("Create a neqw class for practice");
+        
+        System.out.println("Changes something in Github");
     }
 
 }
